ca3aa440347fbf02e5413d8eb4c1dc3cbff31c22
Kleber Stender <kleber.stender@gmail.com>
2015-06-21 14:50:30 -0300

    Criado script de criação do banco do projeto

---
+++ resources/banco.sql
@@ -0,0 +1,9 @@
+create table cliente(
+  nome varchar(15), sobrenome varchar(15), rg char(10), cpf char(11), datanasc date,
+  cep char(8), num int, rua varchar(45), bairro varchar(45), cidade varchar(45), pais char(2)
+);
+
+create table funcionario(
+  nome varchar(15), sobrenome varchar(15), rg char(10), carttrab char(15), datanasc date,
+  cep char(8), num int, rua varchar(45), bairro varchar(45), cidade varchar(45), pais char(2)
+);
